01791787cf6234d37816ba3c9e574ba83c4c8ada
ajmcgrane <145860431+ajmcgrane@users.noreply.github.com>
2023-09-23 17:07:11 -0500

    Update about.py
    Wrote full description

--- pages/about.py
+++ pages/about.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from taipy.gui import Gui
 about_md = """
-TBA
-
+Our project is one of accessibility and safety. HydrOracle utilizes water level data along the Mississippi River from the 
+United States Geological Survey (USGS), and translates it into graphs which can be easily accessed, interpreted, and utilized 
+by everyone. Not only do these graphs provide insight into past trends in water level data, but they also hold predictive capabilities, 
+which provide insight into trends that the future may bring, including the potential for increased flooding. 
+With Hydroracle, anyone from a concerned citizen to a government policymaker can better understand what the future may bring 
+their community, and act accordingly. With this head start, the lives and wellbeing of countless individuals living along the 
+Missippi River may be protected. With HydrOracle, data becomes actionable insights.
 """
 
