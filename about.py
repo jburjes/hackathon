8b4aca4938bd75d521e75a7d86b90b0fa6a08c9d
ntheuerkauf <theuerkaufnicholas@gmail.com~>
2023-09-24 12:39:35 -0500

    reorganization and adding image

--- about.py
+++ about.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 from taipy.gui import Gui
 about_md = """
+<|layout|columns= 3 7|
+<|
+<|{mappic}|image|height=900px|width = 400px|>
+|>
+
+<|
+
 Our project is one of accessibility and safety. HydrOracle utilizes water level data along the Mississippi River from the 
 United States Geological Survey (USGS), and translates it into graphs which can be easily accessed, interpreted, and utilized 
 by everyone. Not only do these graphs provide insight into past trends in water level data, but they also hold predictive capabilities, 
 which provide insight into trends that the future may bring, including the potential for increased flooding. 
 With Hydroracle, anyone from a concerned citizen to a government policymaker can better understand what the future may bring 
 their community, and act accordingly. With this head start, the lives and wellbeing of countless individuals living along the 
-Missippi River may be protected. With HydrOracle, data becomes actionable insights.
+Mississippi River may be protected. With HydrOracle, data becomes actionable insights.
+|>
+|>
 """
 
+mappic = "map.png"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 __name__ == '__main__'
 from taipy.gui import Gui
-from pages.about import about_md
+
 
  
 
@@ -13,6 +13,28 @@ root_md = """
 
 
 """
+about_md = """
+<|layout|columns= 3 7|
+<|
+<|{mappic}|image|height=900px|width = 400px|>
+|>
+
+<|
+
+Our project is one of accessibility and safety. HydrOracle utilizes water level data along the Mississippi River from the 
+United States Geological Survey (USGS), and translates it into graphs which can be easily accessed, interpreted, and utilized 
+by everyone. Not only do these graphs provide insight into past trends in water level data, but they also hold predictive capabilities, 
+which provide insight into trends that the future may bring, including the potential for increased flooding. 
+With Hydroracle, anyone from a concerned citizen to a government policymaker can better understand what the future may bring 
+their community, and act accordingly. With this head start, the lives and wellbeing of countless individuals living along the 
+Mississippi River may be protected. With HydrOracle, data becomes actionable insights.
+|>
+|>
+"""
+
+mappic = "map.png"
+
+
 import VarianceCalc
 from load_data import load_data
 
